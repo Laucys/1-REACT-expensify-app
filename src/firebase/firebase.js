6425869d898459d720de8cbe7c46b8daf982f70d
Dainius Laucys <laucysd@gmail.com>
2020-03-27 09:54:21 +0200

    starting with firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZDx-YFF-bgNFs7LJM1DFwKJ0EYyjLrP8",
+    authDomain: "expensify-1ed95.firebaseapp.com",
+    databaseURL: "https://expensify-1ed95.firebaseio.com",
+    projectId: "expensify-1ed95",
+    storageBucket: "expensify-1ed95.appspot.com",
+    messagingSenderId: "459203522728",
+    appId: "1:459203522728:web:165e7f3a16252260a6929d"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+    name: 'Dainius Laucys'
+});
