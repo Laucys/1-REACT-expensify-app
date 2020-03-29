8e6e9976b2eb5165b724992b640f22f94110e1a2
Dainius Laucys <laucysd@gmail.com>
2020-03-29 11:17:19 +0300

    fetching data from DB

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,29 +14,45 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Dainius Laucys',
-    age: 25,
-    stressLevel: 6,
-    job: {
-        title: 'Software developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Vilnius',
-        country: 'Lithuania'
-    }
-}).then(() => {
-    console.log('Data is saved.');
-}).catch((e) => {
-    console.log('This failed', e);
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
-});
+// database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// });
+
+// database.ref().once('value').then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     }).catch((e) => {
+//         console.log('This failed', e);
+//     });
+
+// database.ref().set({
+//     name: 'Dainius Laucys',
+//     age: 25,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Vilnius',
+//         country: 'Lithuania'
+//     }
+// }).then(() => {
+//     console.log('Data is saved.');
+// }).catch((e) => {
+//     console.log('This failed', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 // database.ref().remove().then(() => {
 //         console.log('Data is removed.');
