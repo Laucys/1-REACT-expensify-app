f0e96f4573caa39ef0b65fe105ca56bf11dc0204
Dainius Laucys <laucysd@gmail.com>
2020-04-08 15:37:57 +0300

    starting integrate firebase with redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,48 +14,4 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().on('value', (snapshot) => {
-    const val = snapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-});
-
-// database.ref().on('value', (snapshot) => {
-//     console.log(snapshot.val());
-// });
-
-// database.ref().once('value').then((snapshot) => {
-//         const val = snapshot.val();
-//         console.log(val);
-//     }).catch((e) => {
-//         console.log('This failed', e);
-//     });
-
-// database.ref().set({
-//     name: 'Dainius Laucys',
-//     age: 25,
-//     stressLevel: 6,
-//     job: {
-//         title: 'Software developer',
-//         company: 'Google'
-//     },
-//     location: {
-//         city: 'Vilnius',
-//         country: 'Lithuania'
-//     }
-// }).then(() => {
-//     console.log('Data is saved.');
-// }).catch((e) => {
-//     console.log('This failed', e);
-// });
-
-// database.ref().update({
-//     stressLevel: 9,
-//     'job/company': 'Amazon',
-//     'location/city': 'Seattle'
-// });
-
-// database.ref().remove().then(() => {
-//         console.log('Data is removed.');
-//     }).catch((e) => {
-//         console.log('This failed', e);
-//     });
+export { firebase, database as default };
