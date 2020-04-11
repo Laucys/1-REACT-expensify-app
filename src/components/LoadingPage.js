cccf9a85dd1d89fbaeaeb6a0bcc5e966ae495fcb
Dainius Laucys <laucysd@gmail.com>
2020-04-11 11:19:51 +0300

    Update LoadingPage.js

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingPage = () => (
   <div className="loader">
-  <img src="/images/preloader_1.gif" />
+  <img src="./images/preloader_1.gif" />
   </div>
 );
 
